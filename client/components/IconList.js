217af2de39242795059386a22740541ad9546c95
Jashanpreet2 <128256855+Jashanpreet2@users.noreply.github.com>
2024-06-29 17:51:33 -0400

    Add social media links

--- client/components/IconList.js
+++ client/components/IconList.js
@@ -5,17 +5,14 @@ export default function IconList() {
     <section className="icon-list pt-5 text-center">
       <p>Follow us to learn more</p>
       <div>
-        <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.linkedin.com/company/seneca-software-developers-club/" target="_blank" rel="noopener noreferrer">
           <MDBIcon fab icon="linkedin-in" size="2x" className="me-4" />
         </a>
-        <a href="https://discord.gg/" target="_blank" rel="noopener noreferrer">
+        <a href="https://discord.gg/zdSsPnYVp4" target="_blank" rel="noopener noreferrer">
           <MDBIcon fab icon="discord" size="2x" className="me-4" />
         </a>
-        <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.instagram.com/seneca_sdc/" target="_blank" rel="noopener noreferrer">
           <MDBIcon fab icon="instagram" size="2x" className="me-4" />
-        </a>
-        <a href="https://www.youtube.com/" target="_blank" rel="noopener noreferrer">
-          <MDBIcon fab icon="youtube" size="2x" className="me-4" />
         </a>
       </div>
     </section>
