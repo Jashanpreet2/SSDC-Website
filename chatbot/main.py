cd6a61cb1914d93e70d0f928174d1af2d03ee9c9
Jashanpreet2 <128256855+Jashanpreet2@users.noreply.github.com>
2024-06-30 14:32:31 -0400

    Chatbot gets news and event context, uses phi3:mini now

--- chatbot/main.py
+++ chatbot/main.py
@@ -1,5 +1,13 @@
+# Runs the server
 from flask import Flask, request
+#Enables cross origin requests
 from flask_cors import CORS, cross_origin
+
+# Gets environment variables
+from dotenv import dotenv_values
+
+# Accesses the mongo database
+from pymongo import MongoClient
 
 # Connects with the LLM
 from langchain_community.llms import Ollama
@@ -30,6 +38,32 @@ app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
+config = dotenv_values('.env.local')
+
+client = MongoClient(config['URI'])
+app.db = client['ssdc-website']
+
+
+# Retrieves all club news
+def get_news():
+    all_news = app.db.news.find({})
+
+    news_context = []
+    for news in all_news:
+        news_context.append(f"Heading: {news['heading']}\nDate:{news['date']}\n{news['content']}")
+
+    return news_context
+
+# Retrieves all club events
+def get_events():
+    all_events = app.db.events.find({})
+
+    event_context = []
+    for event in all_events:
+        event_context.append(f"Heading: {event['heading']}\nDate:{event['date']}\n{event['content']}")
+
+    return event_context
+
 
 """
 Connects with a local Ollama model and answers your questions
@@ -37,38 +71,52 @@ Connects with a local Ollama model and answers your questions
 class Bot:
     """
     parameters:
-        docs: The text that you want to use as context
         model: The locally installed Ollama model's name that you want to connect to
     """
-    def __init__(self, docs, model="llama3"):
+    def __init__(self, model="llama3"):
         # Creating the retrieval chain
-        llm = Ollama(model="llama3")
+        llm = Ollama(model="phi3:mini")
         output_parser = StrOutputParser()
-        prompt = ChatPromptTemplate.from_template("""Answer the following question based on the provided context only:
+        prompt = ChatPromptTemplate.from_template("""Use the following context to help answer any queries.:
 
         <context>
         {context}
         </context>
 
         Question: {input}""")
-        embeddings = OllamaEmbeddings(model="llama3")
+        embeddings = OllamaEmbeddings(model="phi3:mini")
+        news = get_news()
+        events = get_events()
+        all_texts = news + events
         text_splitter = RecursiveCharacterTextSplitter()
-        texts = text_splitter.split_text(docs)
-        vector = FAISS.from_texts(texts, embeddings)
+        texts = text_splitter.split_text("abc")
+        vector = FAISS.from_texts(all_texts, embeddings)
         retriever = vector.as_retriever()
         document_chain = create_stuff_documents_chain(llm, prompt) | output_parser
 
         self.retrieval_chain = create_retrieval_chain(retriever, document_chain)
 
     def ask(self, query):
+        print("asking hte bot")
         return self.retrieval_chain.invoke({"input": query})["answer"]
 
-with open("club.txt", 'r') as f:
-    docs = f.read()
-chatbot = Bot(docs)
+
+# Create the chatbot interface
+app.chatbot = Bot()
+
+"""
+Refreshes the bot's context. Should be used whenever the context is updated. For example, when a new event is posted
+"""
+@app.route('/refresh')
+@cross_origin()
+def refresh():
+    app.chatbot = Bot()
+    return "Bot refreshed successfull!"
+
 
 @app.route('/')
 @cross_origin()
 def ask():
     query = request.args.get("query")
-    return chatbot.ask(query)
+    return app.chatbot.ask(query)
+
