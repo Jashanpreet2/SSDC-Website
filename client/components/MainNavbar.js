713a76179a2d0134cd6c99dcc7fd2febd26a684c
Jashanpreet2 <128256855+Jashanpreet2@users.noreply.github.com>
2024-07-01 10:41:34 -0400

    Console log current page to debug vercel deployment

--- client/components/MainNavbar.js
+++ client/components/MainNavbar.js
@@ -14,6 +14,7 @@ import {
 import Hero from './Hero'
 
 export default function MainNavbar({ currentPage }) {
+  console.log(currentPage)
   const [openNav, setOpenNav] = useState(false)
 
   return (
